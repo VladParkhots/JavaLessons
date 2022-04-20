a6a644059a0f78bb2bff8bcdf4bb31c969b668d6
Uladzislau Parkhats <parhocv@gmail.com>
2022-04-20 17:31:15 +0200

    Day 1: Tasks 4-6

--- src/lessons/one/HomeworkOne.java
+++ src/lessons/one/HomeworkOne.java
@@ -2,21 +2,31 @@ package lessons.one;
 
 public class HomeworkOne {
     public static void main(String[] args) {
-        //Task 1
+        //Task 1/2
         int i =0;
         while (i<10){
             System.out.print("JAVA ");
             i++;
         }
-        //Task 2
+        //Task 3
         for (i=0;i<10;i++){
             System.out.println("JAVA"+i);
         }
-        //Task 3
+        //Task 4
         int w =1980;
         while (w<=2020){
-            System.out.println("ÐžÐ»Ð¸Ð¼Ð¿Ð¸Ð°Ð´Ð° "+w+" Ð³Ð¾Ð´Ð°");
+            System.out.println("Îëèìïèàäà "+w+" ãîäà");
             w=w+4;
         }
+        //Task 5
+        for (i=1980;i<=2020;i=i+4){
+            System.out.println("Îëèìïèàäà "+i+" ãîäà");
+        }
+        //Task 6
+        int k = 3;
+        for (i=1;i<=9;i++){
+            System.out.println(k+" x "+i+" = "+ i*k);
+        }
+
     }
 }
